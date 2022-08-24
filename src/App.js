d03b7db3c23efd2b1515d452882e4fb84abc8bcb
heliodcf <heliodcf@gmail.com>
2022-08-23 20:23:30 -0400

    building API

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import CountryDetails from './components/CountryDetails';
 import Navbar from './components/Navbar';
 import { Routes, Route } from 'react-router-dom';
 import countriesData from './countries.json';
+import axios from 'axios';
+import {useState} from 'react';
 
 function App() {
+const [countries, setCountries] = useState([]);
+
+axios.get('https://ih-countries-api.herokuapp.com/countries').then((result) => {console.log(result)}); 
+
   return (
     <div className="App">
       <Navbar />
@@ -14,14 +20,14 @@ function App() {
           className="col-5"
           style={{ maxHeight: '90vh', overflow: 'scroll' }}
         >
-          <CountriesList />
+          <CountriesList countries={countries}/>
         </div>
 
         <div className="col-7">
           <Routes>
             <Route
               path="/:alpha3code"
-              element={<CountryDetails countries={countriesData} />}
+              element={<CountryDetails countries={countries} />}
             />
           </Routes>
         </div>
